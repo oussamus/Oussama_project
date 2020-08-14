152cddcbbd00108602d369f47d374e894b91c6f4
Oussama <oussamus_84@hotmail.com>
2020-08-14 02:32:06 -0400

    Adding Arduino code for the LED verification. It will send via HC05 an "N" if the LED is ON (either by the push button or by receiving the command from the Android Studio App). It will send an "F" if the LED is OFF.

--- app/src/Arduino/Bluetooth_Arduino_Motor_Code.ino
+++ app/src/Arduino/Bluetooth_Arduino_Motor_Code.ino
@@ -9,15 +9,18 @@ void setup() {
   Serial.begin(9600);
   Serial.println(">> START<<");  
   pinMode(LED,OUTPUT);
+  pinMode(LED_VERIFICATION,INPUT);
 }
 
 void loop() {
+  int reading=0;
+  reading = digitalRead(LED_VERIFICATION );
   while(Serial.available())
   {
-    
+
     char data;
     data = Serial.read();
-    Serial.print(data); 
+    Serial.print(data);
     if(data =='N'){
       digitalWrite(LED,HIGH);
       }
@@ -32,99 +35,8 @@ void loop() {
         Serial.write('F');
         Serial.print('F');
         }
-  
-  
-   /* switch (data)
-    {
-      
-      case '1': //FORWARD
-          digitalWrite(motorControllA, HIGH);
-          digitalWrite(motorControllB, LOW);
-          analogWrite(motorSpeed1, 255);
+     }// while
+     Serial.println(reading);
+     delay(200);
 
-          digitalWrite(motorControllC, HIGH);
-          digitalWrite(motorControllD, LOW);
-          analogWrite(motorSpeed2, 255);
-
-          digitalWrite(motorControllE, HIGH);
-          digitalWrite(motorControllF, LOW);
-          analogWrite(motorSpeed3, 255);
-
-          digitalWrite(motorControllG, HIGH);
-          digitalWrite(motorControllH, LOW);
-          analogWrite(motorSpeed4, 255);
-        break;
-        
-      case '2': //REVERSE
-          digitalWrite(motorControllA, LOW);
-          digitalWrite(motorControllB, HIGH);
-          analogWrite(motorSpeed1, 255);
-
-          digitalWrite(motorControllC, LOW);
-          digitalWrite(motorControllD, HIGH);
-          analogWrite(motorSpeed2, 255);
-
-          digitalWrite(motorControllE, LOW);
-          digitalWrite(motorControllF, HIGH);
-          analogWrite(motorSpeed3, 255);
-
-          digitalWrite(motorControllG, LOW);
-          digitalWrite(motorControllH, HIGH);
-          analogWrite(motorSpeed4, 255);
-        break;
-        
-      case '3': //FORWARD LEFT
-          digitalWrite(motorControllA, HIGH);
-          digitalWrite(motorControllB, LOW);
-          analogWrite(motorSpeed1, 255);
-
-          digitalWrite(motorControllC, HIGH);
-          digitalWrite(motorControllD, LOW);
-          analogWrite(motorSpeed2, 255);
-
-          digitalWrite(motorControllE, LOW);
-          digitalWrite(motorControllF, HIGH);
-          analogWrite(motorSpeed3, 225);
-
-          digitalWrite(motorControllG, LOW);
-          digitalWrite(motorControllH, HIGH);
-          analogWrite(motorSpeed4, 225);
-        break;
-        
-      case '4': //FORWARD RIGHT
-          digitalWrite(motorControllA, LOW);
-          digitalWrite(motorControllB, HIGH);
-          analogWrite(motorSpeed1, 225);
-
-          digitalWrite(motorControllC, LOW);
-          digitalWrite(motorControllD, HIGH);
-          analogWrite(motorSpeed2, 225);
-
-          digitalWrite(motorControllE, HIGH);
-          digitalWrite(motorControllF, LOW);
-          analogWrite(motorSpeed3, 225);
-
-          digitalWrite(motorControllG, HIGH);
-          digitalWrite(motorControllH, LOW);
-          analogWrite(motorSpeed4, 225);
-        break;
-               
-      default: //If bluetooth module receives any value not listed above, both motors turn off
-          digitalWrite(motorControllA, LOW);
-          digitalWrite(motorControllB, LOW);
-          analogWrite(motorSpeed1, 0);
-
-          digitalWrite(motorControllC, LOW);
-          digitalWrite(motorControllD, LOW);
-          analogWrite(motorSpeed2, 0);
-
-          digitalWrite(motorControllE, LOW);
-          digitalWrite(motorControllF, LOW);
-          analogWrite(motorSpeed3, 0);
-
-          digitalWrite(motorControllG, LOW);
-          digitalWrite(motorControllH, LOW);
-          analogWrite(motorSpeed4, 0);
-    }*/
-  }
 }
